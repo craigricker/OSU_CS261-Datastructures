aa29d26ed3f8f9c20770c0eea04ee8bdb2540c7d
Craig <lucke.pirate@gmail.com>
2018-07-11 17:17:56 -0700

    Skeleton of Assignment2

---
+++ Assignment2/stackapp.c
@@ -0,0 +1,55 @@
+/*	stack.c: Stack application. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "dynArray.h"
+
+
+/* ***************************************************************
+Using stack to check for unbalanced parentheses.
+***************************************************************** */
+
+/* Returns the next character of the string, once reaches end return '0' (zero)
+	param: 	s pointer to a string 	
+	pre: s is not null		
+*/
+char nextChar(char* s)
+{
+	static int i = -1;	
+	char c;
+	++i;	
+	c = *(s+i);			
+	if ( c == '\0' )
+		return '\0';	
+	else 
+		return c;
+}
+
+/* Checks whether the (), {}, and [] are balanced or not
+	param: 	s pointer to a string 	
+	pre: s is not null	
+	post:	
+*/
+int isBalanced(char* s)
+{
+	/* FIXME: You will write this function */		
+	return 0;
+}
+
+int main(int argc, char* argv[]){
+	
+	char* s=argv[1];	
+	int res;
+	
+	printf("Assignment 2\n");
+
+	res = isBalanced(s);
+
+	if (res)
+		printf("The string %s is balanced\n",s);
+	else 
+		printf("The string %s is not balanced\n",s);
+	
+	return 0;	
+}
+
